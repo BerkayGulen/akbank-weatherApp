c7a127e3b5f6222c180d3094705ff61d9dcc7667
BerkayGulen <berkaygulen2@gmail.com>
2023-05-30 23:02:31 +0300

    First commit
    - Spring Boot project created
    - Dependencies added

---
+++ src/main/java/com/berkaygulen/akbankweatherApp/AkbankWeatherAppApplication.java
@@ -0,0 +1,13 @@
+package com.berkaygulen.akbankweatherApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkbankWeatherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkbankWeatherAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/berkaygulen/akbankweatherApp/AkbankWeatherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berkaygulen.akbankweatherApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkbankWeatherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
