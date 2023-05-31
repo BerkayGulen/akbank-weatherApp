0156ae99cd6031f16319a6cd1e7a35778023b088
BerkayGulen <berkaygulen2@gmail.com>
2023-05-31 21:06:14 +0300

    Bootstrap dependencies added

--- src/main/frontend/src/UserSignUpPage.js
+++ src/main/frontend/src/UserSignUpPage.js
@@ -13,56 +13,58 @@ class UserSignUpPage extends React.Component {
         password: null,
         passwordRepeat: null
     }
-    onChange = event =>{
-      const {name,value} = event.target;
-      this.setState({
-          [name]:value
-      })
+    onChange = event => {
+        const {name, value} = event.target;
+        this.setState({
+            [name]: value
+        })
     };
 
-     onClickSignUp = event=> {
+    onClickSignUp = event => {
         event.preventDefault();
-        const {username,firstName,lastName,email,displayName,password} = this.state;
-        const body = {username ,firstName,lastName,email,password};
+        const {username, firstName, lastName, email, password} = this.state;
+        const body = {username, firstName, lastName, email, password};
 
-        axios.post("/api/v1/users",body)
+        axios.post("/api/v1/users", body)
     }
+
     render() {
         return (
-            <form>
-                <h1>Sign up</h1>
-                <div>
-                    <label>Username</label>
-                    <input name="username" onChange={this.onChange}/>
-                </div>
-                <div>
-                    <label>Display Name</label>
-                    <input name="firstName" onChange={this.onChange}/>
-                </div>
-                <div>
-                    <label>Display Name</label>
-                    <input name="lastName" onChange={this.onChange}/>
-                </div>
-                <div>
-                    <label>Display Name</label>
-                    <input name="email" onChange={this.onChange}/>
-                </div>
-                {/*<div>*/}
-                {/*    <label>Display Name</label>*/}
-                {/*    <input name="displayName" onChange={this.onChange}/>*/}
-                {/*</div>*/}
-                <div>
-                    <label>Password</label>
-                    <input name="password" type={"password"} onChange={this.onChange}/>
-                </div>
-                <div>
-                    <label>Password Repeat</label>
-                    <input name="passwordRepeat" type={"password"} onChange={this.onChange}/>
-                </div>
-                <button onClick={this.onClickSignUp}>Sign Up</button>
+            <div className="container">
+                <form>
+                    <h1 className="text-center">Sign up</h1>
+                    <div className="form-group">
+                        <label >Username</label>
+                        <input className={"form-control"} name="username" onChange={this.onChange}/>
+                    </div>
+                    <div>
+                        <label>Display Name</label>
+                        <input className={"form-control"} name="firstName" onChange={this.onChange}/>
+                    </div>
+                    <div>
+                        <label>Display Name</label>
+                        <input className={"form-control"} name="lastName" onChange={this.onChange}/>
+                    </div>
+                    <div>
+                        <label>Display Name</label>
+                        <input className={"form-control"} name="email" onChange={this.onChange}/>
+                    </div>
+                    <div>
+                        <label>Password</label>
+                        <input className={"form-control"} name="password" type={"password"} onChange={this.onChange}/>
+                    </div>
+                    <div>
+                        <label>Password Repeat</label>
+                        <input className={"form-control"} name="passwordRepeat" type={"password"} onChange={this.onChange}/>
+                    </div>
+                    <div className={"text-center "}>
+                        <button className={"btn btn-primary"} onClick={this.onClickSignUp}>Sign Up</button>
+                    </div>
 
 
-            </form>
+                </form>
+
+            </div>
         );
     }
 }
